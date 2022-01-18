0f9fe794cf590a61600ee5ea9a80ad592ffeca7a
TauPhoton <the-forgotten_one@hotmail.com>
2022-01-17 20:42:57 -0600

    initiating atbash python code

---
+++ atbash.py
@@ -0,0 +1,16 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+permanency = True
+
+while permanency:
+    print ("""encrypt[e]
+              decrypt[d]
+              
+              """)
+    menu_option = input('enter an option:_ ').lower()
+    if menu_option == 'e':
+        pass
+    elif menu_option == 'd':
+        pass
+    permanency = False
